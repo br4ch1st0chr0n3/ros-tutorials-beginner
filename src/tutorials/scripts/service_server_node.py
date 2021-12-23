71554bced7beac7acb76fd58090a7aa2d1605a70
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-12-23 17:22:13 +0300

    initial commit

---
+++ src/tutorials/scripts/service_server_node.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import rospy
+from tutorials.srv import multiplier, multiplierResponse
+
+def callback(request):
+    return multiplierResponse(request.a * request.b)
+
+def multiply():
+    rospy.init_node("multiplier_service")
+    service = rospy.Service("multiplier", multiplier, callback)
+    rospy.spin()
+
+if __name__ == "__main__":
+    multiply()
